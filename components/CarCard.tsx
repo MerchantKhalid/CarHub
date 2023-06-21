935ab2e03eda13b2bb12bffc660dde33de6efde8
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2023-06-21 16:51:36 +0200

    car catalogue

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -14,8 +14,10 @@ const CarCard = ({ car }: CarCardProps) => {
           {make} {model}
         </h2>
       </div>
-      <p>
-        <span>{carRent}</span>
+      <p className="flex mt-6 text-[32px] font-extrabold">
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
       </p>
     </div>
   );
